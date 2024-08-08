2923461e29bc4007030864078b5b9ace80ceb9ea
Bittu Kumar <bk7355583@gmail.com>
2024-08-08 21:13:55 +0530

    timmer application added.

---
+++ 01_Google_Timmer/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Timmer  from './Timer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+      <div >
+      <Timmer/>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
