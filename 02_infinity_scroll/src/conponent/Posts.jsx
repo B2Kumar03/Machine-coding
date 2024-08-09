abd31e14406126f93d94ab5976c8aaaadd832d17
Bittu Kumar <bk7355583@gmail.com>
2024-08-09 19:21:37 +0530

    Edited

--- 02_infinity_scroll/src/conponent/Posts.jsx
+++ 02_infinity_scroll/src/conponent/Posts.jsx
@@ -30,7 +30,7 @@ const Posts = () => {
   
     if(innerHeight+scrollTop+1>=scrollHeight){
         setPage((prev)=>prev+1);
-      }  //if user scrolls to the bottom of the page, it will fetch more posts.  //you can adjust the condition as per your requirement.  //this is just a simple example. You can also add error handling to this function.  //also, you may want to debounce this function to prevent unnecessary API calls.  //you can use lodash.debounce function for this.  //https://lodash.com/docs/4.17.15#debounce  //this example uses the window.addEventListener method to listen for scroll events. You can also use Intersection Observer API if you want to use native browser features.  //https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API  //https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView  //https://developer.mozilla.
+      } 
     
   };
   useEffect(() => {
